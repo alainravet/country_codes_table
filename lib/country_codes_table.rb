4081d4ea99e28c87eaf3cb6e26725ebe88946b19
Alain Ravet <alainravet@gmail.com>
2015-07-10 23:59:32 +0200

    11 versions of CountryCodesTable.with_*(<code>)

--- lib/country_codes_table.rb
+++ lib/country_codes_table.rb
@@ -1,5 +1,106 @@
 require "country_codes_table/version"
+require 'csv'
 
 module CountryCodesTable
-  # Your code goes here...
+  class CountryDetails < Struct.new(:name, :alpha2, :alpha3, :numeric, :itu, :gec, :ioc, :fifa, :ds, :wmo, :gaul, :marc, :dial, :independent)
+
+    def self.all
+      CountryCodesTable._dire.map{|args| CountryDetails.new(*args) }
+    end
+  end
+
+  def self._dire
+    @_dire ||= CSV.parse(File.open(File.expand_path('country_codes_table/data.csv', __dir__)))
+  end
+
+  def self._raw_data
+    @_raw_data ||= _dire.transpose
+  end
+
+  def self.with_alpha_2(code)
+    @_alpha2_to_idx ||= raw_data_to_index(1)
+    _countries_details(@_alpha2_to_idx[code])
+  end
+
+  def self.raw_data_to_index(selector)
+    res = {}
+    _raw_data[selector].each_with_index { |obj , idx |
+      next unless obj
+      if res[obj].nil?
+        res[obj] = idx
+      elsif res[obj].is_a?(Array)
+        res[obj] << idx
+      else
+        res[obj] = [res[obj]]
+        res[obj] << idx
+      end
+
+    }
+    res
+  end
+
+  def self.with_alpha_3(code)
+    @_alpha3_to_idx ||= raw_data_to_index(2)
+    _countries_details(@_alpha3_to_idx[code])
+  end
+
+  def self.with_numeric(code)
+    @_numeric_to_idx ||= raw_data_to_index(3)
+    _countries_details(@_numeric_to_idx[code])
+  end
+
+  def self.with_itu(code)
+    @_numeric_to_itu ||= raw_data_to_index(4)
+    _countries_details(@_numeric_to_itu[code])
+  end
+
+  def self.with_gec(code)
+    @_numeric_to_gec ||= raw_data_to_index(5)
+    _countries_details(@_numeric_to_gec[code])
+  end
+
+  def self.with_ioc(code)
+    @_numeric_to_ioc ||= raw_data_to_index(6)
+    key = @_numeric_to_ioc[code]
+    _countries_details(key)
+  end
+
+  def self._countries_details(keys)
+    if keys.is_a?(Array)
+      keys.map{|key| CountryDetails.all[key]}
+    else
+      CountryDetails.all[keys]
+    end
+  end
+
+  def self.with_fifa(code)
+    @_numeric_to_fifa ||= raw_data_to_index(7)
+    _countries_details(@_numeric_to_fifa[code])
+  end
+
+  def self.with_ds(code)
+    @_numeric_to_ds ||= raw_data_to_index(8)
+    _countries_details(@_numeric_to_ds[code])
+  end
+
+  def self.with_wmo(code)
+    @_numeric_to_wmo ||= raw_data_to_index(9)
+    _countries_details(@_numeric_to_wmo[code])
+  end
+
+  def self.with_gaul(code)
+    @_numeric_to_gaul ||= raw_data_to_index(10)
+    _countries_details(@_numeric_to_gaul[code])
+  end
+
+  def self.with_marc(code)
+    @_numeric_to_marc||= raw_data_to_index(11)
+    _countries_details(@_numeric_to_marc[code])
+  end
+
+  def self.with_dial(code)
+    @_numeric_to_dial ||= raw_data_to_index(12)
+    _countries_details(@_numeric_to_dial[code])
+  end
+
 end
--- test/country_codes_table_test.rb
+++ test/country_codes_table_test.rb
@@ -1,11 +1,85 @@
-require 'test_helper'
+# data source : http://www.statoids.com/wab.html
+
+require_relative 'test_helper'
 
 class CountryCodesTableTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::CountryCodesTable::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_finding_by_ISO_3166_1_Alpha_2_code
+    details = CountryCodesTable.with_alpha_2('AF')
+    assert_equal 'Afghanistan',   details.name
+    assert_equal 'AF',            details.alpha2
   end
+
+  def test_finding_by_ISO_3166_1_Alpha_3_code
+    details = CountryCodesTable.with_alpha_3('DZA')
+    assert_equal 'Algeria',   details.name
+    assert_equal 'DZA',       details.alpha3
+  end
+
+  def test_finding_by_ISO_3166_1_Numeric_code
+    details = CountryCodesTable.with_numeric('008')
+    assert_equal 'Albania',   details.name
+    assert_equal '008',       details.numeric
+  end
+
+  def test_finding_by_ITU_code
+    details = CountryCodesTable.with_itu('AGL')
+    assert_equal 'Angola',    details.name
+    assert_equal 'AGL',       details.itu
+  end
+
+
+  def test_finding_by_GEC_code
+    details = CountryCodesTable.with_gec('AY')
+    assert_equal 'Antarctica', details.name
+    assert_equal 'AY',         details.gec
+  end
+
+  def test_finding_by_IOC_code
+    details = CountryCodesTable.with_ioc('GEQ')
+    assert_equal 'Equatorial Guinea', details.name
+    assert_equal 'GEQ',               details.ioc
+  end
+
+  def test_finding_by_FIFA_code
+    details = CountryCodesTable.with_fifa('ROS3')
+    assert_equal 'Antarctica',  details.name
+    assert_equal 'ROS3',        details.fifa
+  end
+
+  def test_finding_by_DS_code
+    details = CountryCodesTable.with_ds('RA')
+    assert_equal 'Argentina', details.name
+    assert_equal 'RA',        details.ds
+  end
+
+  def test_finding_by_wmo_code
+    details = CountryCodesTable.with_wmo('NU')
+    assert_equal ["Aruba", "Bonaire, Sint Eustatius and Saba"],   details.map(&:name)
+    assert_equal ["NU", "NU"],                                    details.map(&:wmo)
+  end
+
+  def test_finding_by_gaul_code
+    details = CountryCodesTable.with_gaul('17')
+    assert_equal 'Australia', details.name
+    assert_equal '17',        details.gaul
+  end
+
+  def test_finding_by_marc_code
+    details = CountryCodesTable.with_marc('aa')
+    assert_equal 'Albania', details.name
+    assert_equal 'aa',      details.marc
+  end
+
+  def test_finding_by_dial_code
+    details = CountryCodesTable.with_dial('1-242')
+    assert_equal 'Bahamas', details.name
+    assert_equal '1-242',   details.dial
+  end
+
+  #TODO : raise NotFoundError when ..
+  #TODO : better handling of results being collections
 end
