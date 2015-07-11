b1ce85396c26b20d2d4d432a74c3e99ef26bba9f
Alain Ravet <alainravet@gmail.com>
2015-07-11 15:05:14 +0200

    11 versions of CountryCodesTable.to_*(<code>)

--- lib/country_codes_table.rb
+++ lib/country_codes_table.rb
@@ -2,11 +2,40 @@ require "country_codes_table/version"
 require 'csv'
 
 module CountryCodesTable
-  class CountryDetails < Struct.new(:name, :alpha2, :alpha3, :numeric, :itu, :gec, :ioc, :fifa, :ds, :wmo, :gaul, :marc, :dial, :independent)
+  SELECTOR_A2   = 1
+  SELECTOR_A3   = 2
+  SELECTOR_NUM  = 3
+  SELECTOR_ITU  = 4
+  SELECTOR_GEC  = 5
+  SELECTOR_IOC  = 6
+  SELECTOR_FIFA = 7
+  SELECTOR_DS   = 8
+  SELECTOR_WMO  = 9
+  SELECTOR_GAUL = 10
+  SELECTOR_MARC = 11
+  SELECTOR_DIAL = 12
+
+  class CountryDetails < Struct.new(:name, :alpha2, :alpha3, :numeric, :itu, :gec, :ioc, :fifa, :ds, :wmo, :gaul, :marc, :dial, :independent, :idx)
 
     def self.all
-      CountryCodesTable._dire.map{|args| CountryDetails.new(*args) }
+      a = CountryCodesTable._dire.each_with_index.map{|args, idx|
+        CountryDetails.new(*args, idx )
+      }
+      a
     end
+
+    def to_alpha_2 ; CountryCodesTable._raw_data[SELECTOR_A2  ][self.idx] end
+    def to_alpha_3 ; CountryCodesTable._raw_data[SELECTOR_A3  ][self.idx] end
+    def to_numeric ; CountryCodesTable._raw_data[SELECTOR_NUM ][self.idx] end
+    def to_itu     ; CountryCodesTable._raw_data[SELECTOR_ITU ][self.idx] end
+    def to_gec     ; CountryCodesTable._raw_data[SELECTOR_GEC ][self.idx] end
+    def to_ioc     ; CountryCodesTable._raw_data[SELECTOR_IOC ][self.idx] end
+    def to_fifa    ; CountryCodesTable._raw_data[SELECTOR_FIFA][self.idx] end
+    def to_ds      ; CountryCodesTable._raw_data[SELECTOR_DS  ][self.idx] end
+    def to_wmo     ; CountryCodesTable._raw_data[SELECTOR_WMO ][self.idx] end
+    def to_gaul    ; CountryCodesTable._raw_data[SELECTOR_GAUL][self.idx] end
+    def to_marc    ; CountryCodesTable._raw_data[SELECTOR_MARC][self.idx] end
+    def to_dial    ; CountryCodesTable._raw_data[SELECTOR_DIAL][self.idx] end
   end
 
   def self._dire
@@ -17,9 +46,13 @@ module CountryCodesTable
     @_raw_data ||= _dire.transpose
   end
 
-  def self.with_alpha_2(code)
-    @_alpha2_to_idx ||= raw_data_to_index(1)
-    _countries_details(@_alpha2_to_idx[code])
+
+  def self._countries_details(keys)
+    if keys.is_a?(Array)
+      keys.map{|key| CountryDetails.all[key]}
+    else
+      CountryDetails.all[keys]
+    end
   end
 
   def self.raw_data_to_index(selector)
@@ -34,73 +67,68 @@ module CountryCodesTable
         res[obj] = [res[obj]]
         res[obj] << idx
       end
-
     }
     res
   end
 
+  def self.with_alpha_2(code)
+    @_alpha2_to_idx ||= raw_data_to_index(SELECTOR_A2)
+    _countries_details(@_alpha2_to_idx[code])
+  end
+
   def self.with_alpha_3(code)
-    @_alpha3_to_idx ||= raw_data_to_index(2)
+    @_alpha3_to_idx ||= raw_data_to_index(SELECTOR_A3)
     _countries_details(@_alpha3_to_idx[code])
   end
 
   def self.with_numeric(code)
-    @_numeric_to_idx ||= raw_data_to_index(3)
+    @_numeric_to_idx ||= raw_data_to_index(SELECTOR_NUM)
     _countries_details(@_numeric_to_idx[code])
   end
 
   def self.with_itu(code)
-    @_numeric_to_itu ||= raw_data_to_index(4)
+    @_numeric_to_itu ||= raw_data_to_index(SELECTOR_ITU)
     _countries_details(@_numeric_to_itu[code])
   end
 
   def self.with_gec(code)
-    @_numeric_to_gec ||= raw_data_to_index(5)
+    @_numeric_to_gec ||= raw_data_to_index(SELECTOR_GEC)
     _countries_details(@_numeric_to_gec[code])
   end
 
   def self.with_ioc(code)
-    @_numeric_to_ioc ||= raw_data_to_index(6)
+    @_numeric_to_ioc ||= raw_data_to_index(SELECTOR_IOC)
     key = @_numeric_to_ioc[code]
     _countries_details(key)
   end
 
-  def self._countries_details(keys)
-    if keys.is_a?(Array)
-      keys.map{|key| CountryDetails.all[key]}
-    else
-      CountryDetails.all[keys]
-    end
-  end
-
   def self.with_fifa(code)
-    @_numeric_to_fifa ||= raw_data_to_index(7)
+    @_numeric_to_fifa ||= raw_data_to_index(SELECTOR_FIFA)
     _countries_details(@_numeric_to_fifa[code])
   end
 
   def self.with_ds(code)
-    @_numeric_to_ds ||= raw_data_to_index(8)
+    @_numeric_to_ds ||= raw_data_to_index(SELECTOR_DS)
     _countries_details(@_numeric_to_ds[code])
   end
 
   def self.with_wmo(code)
-    @_numeric_to_wmo ||= raw_data_to_index(9)
+    @_numeric_to_wmo ||= raw_data_to_index(SELECTOR_WMO)
     _countries_details(@_numeric_to_wmo[code])
   end
 
   def self.with_gaul(code)
-    @_numeric_to_gaul ||= raw_data_to_index(10)
+    @_numeric_to_gaul ||= raw_data_to_index(SELECTOR_GAUL)
     _countries_details(@_numeric_to_gaul[code])
   end
 
   def self.with_marc(code)
-    @_numeric_to_marc||= raw_data_to_index(11)
+    @_numeric_to_marc||= raw_data_to_index(SELECTOR_MARC)
     _countries_details(@_numeric_to_marc[code])
   end
 
   def self.with_dial(code)
-    @_numeric_to_dial ||= raw_data_to_index(12)
+    @_numeric_to_dial ||= raw_data_to_index(SELECTOR_DIAL)
     _countries_details(@_numeric_to_dial[code])
   end
-
 end
--- test/country_codes_table_test.rb
+++ test/country_codes_table_test.rb
@@ -13,11 +13,23 @@ class CountryCodesTableTest < Minitest::Test
     assert_equal 'AF',            details.alpha2
   end
 
+  def test_converting_ISO_3166_1_Alpha_2_code
+    assert_equal 'AL',   CountryCodesTable.with_alpha_2('AL' ).to_alpha_2
+    assert_equal 'DZ',   CountryCodesTable.with_alpha_3('DZA').to_alpha_2
+  end
+
+
   def test_finding_by_ISO_3166_1_Alpha_3_code
     details = CountryCodesTable.with_alpha_3('DZA')
     assert_equal 'Algeria',   details.name
     assert_equal 'DZA',       details.alpha3
   end
+
+  def test_converting_ISO_3166_1_Alpha_3_code
+    assert_equal 'ALB',   CountryCodesTable.with_alpha_2('AL'  ).to_alpha_3
+    assert_equal 'DZA',   CountryCodesTable.with_alpha_3('DZA' ).to_alpha_3
+  end
+
 
   def test_finding_by_ISO_3166_1_Numeric_code
     details = CountryCodesTable.with_numeric('008')
@@ -25,10 +37,21 @@ class CountryCodesTableTest < Minitest::Test
     assert_equal '008',       details.numeric
   end
 
+  def test_converting_ISO_3166_1_Numeric_code
+    assert_equal '008',   CountryCodesTable.with_alpha_2('AL'  ).to_numeric
+    assert_equal '008',   CountryCodesTable.with_numeric('008' ).to_numeric
+  end
+
+
   def test_finding_by_ITU_code
     details = CountryCodesTable.with_itu('AGL')
     assert_equal 'Angola',    details.name
     assert_equal 'AGL',       details.itu
+  end
+
+  def test_converting_ITU_code
+    assert_equal 'ALB',   CountryCodesTable.with_alpha_2('AL' ).to_itu
+    assert_equal 'AGL',   CountryCodesTable.with_itu('AGL'    ).to_itu
   end
 
 
@@ -38,11 +61,23 @@ class CountryCodesTableTest < Minitest::Test
     assert_equal 'AY',         details.gec
   end
 
+  def test_converting_GEC_code
+    assert_equal 'AL',   CountryCodesTable.with_alpha_2('AL').to_gec
+    assert_equal 'AY',   CountryCodesTable.with_gec('AY'    ).to_gec
+  end
+
+
   def test_finding_by_IOC_code
     details = CountryCodesTable.with_ioc('GEQ')
     assert_equal 'Equatorial Guinea', details.name
     assert_equal 'GEQ',               details.ioc
   end
+
+  def test_converting_IOC_code
+    assert_equal 'ALB',   CountryCodesTable.with_alpha_2('AL').to_ioc
+    assert_equal 'GEQ',   CountryCodesTable.with_ioc('GEQ'    ).to_ioc
+  end
+
 
   def test_finding_by_FIFA_code
     details = CountryCodesTable.with_fifa('ROS3')
@@ -50,11 +85,23 @@ class CountryCodesTableTest < Minitest::Test
     assert_equal 'ROS3',        details.fifa
   end
 
+  def test_converting_to_FIFA_code
+    assert_equal 'ALB',   CountryCodesTable.with_alpha_2('AL' ).to_fifa
+    assert_equal 'ROS3',  CountryCodesTable.with_fifa('ROS3'  ).to_fifa
+  end
+
+
   def test_finding_by_DS_code
     details = CountryCodesTable.with_ds('RA')
     assert_equal 'Argentina', details.name
     assert_equal 'RA',        details.ds
   end
+
+  def test_converting_to_DS_code
+    assert_equal 'AL',   CountryCodesTable.with_alpha_2('AL' ).to_ds
+    assert_equal 'RA',  CountryCodesTable.with_ds('RA'  ).to_ds
+  end
+
 
   def test_finding_by_wmo_code
     details = CountryCodesTable.with_wmo('NU')
@@ -62,10 +109,21 @@ class CountryCodesTableTest < Minitest::Test
     assert_equal ["NU", "NU"],                                    details.map(&:wmo)
   end
 
+  def test_converting_to_WMO_code
+    assert_equal 'AB',   CountryCodesTable.with_alpha_2('AL' ).to_wmo
+    assert_equal ['NU','NU'],  CountryCodesTable.with_wmo('NU'  ).map(&:to_wmo)
+  end
+
+
   def test_finding_by_gaul_code
     details = CountryCodesTable.with_gaul('17')
     assert_equal 'Australia', details.name
     assert_equal '17',        details.gaul
+  end
+
+  def test_converting_to_GAUL_code
+    assert_equal '3',   CountryCodesTable.with_alpha_2('AL').to_gaul
+    assert_equal '17',  CountryCodesTable.with_gaul('17'   ).to_gaul
   end
 
   def test_finding_by_marc_code
@@ -74,11 +132,22 @@ class CountryCodesTableTest < Minitest::Test
     assert_equal 'aa',      details.marc
   end
 
+  def test_converting_to_MARC_code
+    assert_equal 'aa',   CountryCodesTable.with_alpha_2('AL').to_marc
+    assert_equal 'aa',  CountryCodesTable.with_marc('aa'   ).to_marc
+  end
+
   def test_finding_by_dial_code
     details = CountryCodesTable.with_dial('1-242')
     assert_equal 'Bahamas', details.name
     assert_equal '1-242',   details.dial
   end
+
+  def test_converting_to_DIAL_code
+    assert_equal '355',   CountryCodesTable.with_alpha_2('AL').to_dial
+    assert_equal '1-242', CountryCodesTable.with_dial('1-242').to_dial
+  end
+
 
   #TODO : raise NotFoundError when ..
   #TODO : better handling of results being collections
